698e1d1bd513e465b9dba50c63241d5cd1023f2f
Randy Guthrie <randy.guthrie@hotmail.com>
2018-09-06 16:12:05 -0600

    Added Screen Title

---
+++ Energy Measure Converter/JoulesToCalorie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Energy_Measure_Converter
+{
+    public partial class JoulesToCalorie : Form
+    {
+        public JoulesToCalorie()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
